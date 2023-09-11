177c46a649f70f0a718347f376fbe884134634c0
Austin Bao <austin_dun@outlook.com>
2023-09-11 01:35:05 -0600

    cyu1 for 03

---
+++ 03/CYU-1/main.js
@@ -0,0 +1,24 @@
+// CS10 GRADE CALC BY AUSTIN
+
+// Button event listener
+document.getElementById("analyzebtn").addEventListener("click", getNumberInfo);
+
+function getNumberInfo() {
+  // Grabbing input
+  let number = +document.getElementById("number").value;
+
+  // Processing & displaying it on html
+  if (number % 2 === 0) {
+    document.getElementById("evenodd").innerHTML = "Even";
+  } else {
+    document.getElementById("evenodd").innerHTML = "Odd";
+  }
+
+  if (number > 0) {
+    document.getElementById("posnegzero").innerHTML = "Positive";
+  } else if (number < 0) {
+    document.getElementById("posnegzero").innerHTML = "Negative";
+  } else {
+    document.getElementById("posnegzero").innerHTML = "Zero";
+  }
+}
