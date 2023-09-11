253f28a8ad98148a7dfb27d9cb2f0dc76da4e486
Austin Bao <austin_dun@outlook.com>
2023-09-10 23:14:00 -0600

    organized files

--- main.py
+++
@@ -1,9 +0,0 @@
-data = {"dict1":{"id":0,"name": "A"},
-        "dict2":{"id":1,"name": "B"},
-        "dict3":{"id":2, "name": "C"}}
-
-
-for i in data.keys():
-    for j in data[i]:
-        print(data[i][j])
-
