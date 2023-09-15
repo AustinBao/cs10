9447eefdb41b2737203449c1a81514b150362640
Austin Bao <austin_dun@outlook.com>
2023-09-14 22:47:49 -0600

    quiz complete

--- 04/Magic8Ball/main.js
+++ 04/Magic8Ball/main.js
@@ -7,21 +7,46 @@ let result = document.getElementById("result");
 
 function magic8Ball() {
   let randnum = Math.random();
+  // MADE IT CASE INSENSITIVE
+  let question = document.getElementById("question").value.toLowerCase();
 
-  if (randnum < 0.2) {
-    result.innerHTML = "Without a Doubt.";
-    result.style.color = "green";
-  } else if (randnum < 0.4) {
-    result.innerHTML = "As I see it, yes.";
-    result.style.color = "green";
-  } else if (randnum < 0.6) {
-    result.innerHTML = "Concentrate and ask again.";
+  // SPECIFIC QUESTION. RETURN SO THE OTHER IF STATEMENTS DONT RUN
+  if (question === "is harry ainlay the best school?") {
+    result.innerHTML = "Obviously Harry Ainlay School is the best!";
+    result.style.color = "blue";
+    return;
+  }
+  if (question === "is computer science cool?") {
+    result.innerHTML = "Of course it is";
+    result.style.color = "yellow";
+    return;
+  }
+  if (question === "is water good for you?") {
+    result.innerHTML = "You need water to survive so yeah";
+    result.style.color = "orange";
+    return;
+  }
+
+  // CHECKS IF THE INPUT IS EMPTY
+  if (question.length === 0) {
+    result.innerHTML = "Please ask a question...";
     result.style.color = "black";
-  } else if (randnum < 0.8) {
-    result.innerHTML = "Don't count on it.";
-    result.style.color = "red";
   } else {
-    result.innerHTML = "Outlook not so good.";
-    result.style.color = "red";
+    if (randnum < 0.2) {
+      result.innerHTML = "Without a Doubt.";
+      result.style.color = "green";
+    } else if (randnum < 0.4) {
+      result.innerHTML = "As I see it, yes.";
+      result.style.color = "green";
+    } else if (randnum < 0.6) {
+      result.innerHTML = "Concentrate and ask again.";
+      result.style.color = "black";
+    } else if (randnum < 0.8) {
+      result.innerHTML = "Don't count on it.";
+      result.style.color = "red";
+    } else {
+      result.innerHTML = "Outlook not so good.";
+      result.style.color = "red";
+    }
   }
 }
--- 04/Quiz/main.js
+++ 04/Quiz/main.js
@@ -1,6 +1,89 @@
 // CS10 QUIZ BY AUSTIN
 
 // Button event listener
-document.getElementById("btn").addEventListener("click", quizResults);
+document.getElementById("submit").addEventListener("click", quizResults);
 
-function quizResults() {}
+function quizResults() {
+  let google = document.getElementById("google").value.toLowerCase();
+  let pin = document.getElementById("pin").value.toLowerCase();
+  let cookie = document.getElementById("cookie").value.toLowerCase();
+  let wirelessfidelity = document
+    .getElementById("wirelessfidelity")
+    .value.toLowerCase();
+  let android = document.getElementById("android").value.toLowerCase();
+  let faang = document.getElementById("faang").value.toLowerCase();
+  let leagueoflegends = document
+    .getElementById("leagueoflegends")
+    .value.toLowerCase();
+
+  let score = 0;
+
+  if (google === "google") {
+    score++;
+    document.getElementById("google").style.outline = "2px solid green";
+  } else {
+    document.getElementById("google").style.outline = "2px solid red";
+  }
+  if (pin === "personal identification number") {
+    score++;
+    document.getElementById("pin").style.outline = "2px solid green";
+  } else {
+    document.getElementById("pin").style.outline = "2px solid red";
+  }
+  if (cookie === "cookie") {
+    score++;
+    document.getElementById("cookie").style.outline = "2px solid green";
+  } else {
+    document.getElementById("cookie").style.outline = "2px solid red";
+  }
+  if (wirelessfidelity === "wireless fidelity") {
+    score++;
+    document.getElementById("wirelessfidelity").style.outline =
+      "2px solid green";
+  } else {
+    document.getElementById("wirelessfidelity").style.outline = "2px solid red";
+  }
+  if (android === "android") {
+    score++;
+    document.getElementById("android").style.outline = "2px solid green";
+  } else {
+    document.getElementById("android").style.outline = "2px solid red";
+  }
+  if (
+    faang === "facebook" ||
+    faang === "apple" ||
+    faang === "amazon" ||
+    faang === "netflix" ||
+    faang === "google"
+  ) {
+    score++;
+    document.getElementById("faang").style.outline = "2px solid green";
+  } else {
+    document.getElementById("faang").style.outline = "2px solid red";
+  }
+  if (leagueoflegends === "league of legends") {
+    score++;
+    document.getElementById("leagueoflegends").style.outline =
+      "2px solid green";
+  } else {
+    document.getElementById("leagueoflegends").style.outline = "2px solid red";
+  }
+
+  let percent = Math.round((score / 7) * 100);
+
+  document.getElementById("result").innerHTML = `${score}/7 or ${percent}%`;
+
+  if (score >= 6) {
+    document.getElementById("encourage").innerHTML = "Very well done!";
+    document.getElementById("encourage").style.color = "green";
+  } else if (score >= 4) {
+    document.getElementById("encourage").innerHTML = "Not bad!";
+    document.getElementById("encourage").style.color = "lime";
+  } else if (score >= 2) {
+    document.getElementById("encourage").innerHTML = "Keep trying!";
+    document.getElementById("encourage").style.color = "orange";
+  } else {
+    document.getElementById("encourage").innerHTML = "You need to study more!";
+    document.getElementById("encourage").style.color = "red";
+  }
+}
