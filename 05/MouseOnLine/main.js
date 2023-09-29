aad931f7743b6b649eb0b025d0c0b0f66238cc89
Austin Bao <austin_dun@outlook.com>
2023-09-28 20:17:40 -0600

    moving square

--- 05/MouseOnLine/main.js
+++ 05/MouseOnLine/main.js
@@ -2,82 +2,63 @@
 
 // GET CANVAS
 let cnv = document.getElementById("mycanvas");
-cnv.width = cnv.height = 400;
+cnv.width = 400;
+cnv.height = 400;
 let ctx = cnv.getContext("2d");
 
-// draw
-ctx.moveTo(40, 0);
-ctx.lineTo(40, 360);
+// DRAW MAZE
+ctx.fillStyle = "green";
+ctx.fillRect(5, 5, 50, 50);
+
+ctx.moveTo(60, 0);
+ctx.lineTo(60, 340);
 ctx.stroke();
-ctx.moveTo(80, 400);
-ctx.lineTo(80, 40);
+ctx.moveTo(120, 400);
+ctx.lineTo(120, 60);
 ctx.stroke();
-ctx.moveTo(120, 0);
-ctx.lineTo(120, 360);
+ctx.moveTo(180, 0);
+ctx.lineTo(180, 340);
 ctx.stroke();
-ctx.moveTo(160, 400);
-ctx.lineTo(160, 40);
+ctx.moveTo(240, 400);
+ctx.lineTo(240, 60);
 ctx.stroke();
-ctx.moveTo(0, 100);
-ctx.lineTo(400, 100);
+ctx.moveTo(300, 0);
+ctx.lineTo(300, 340);
 ctx.stroke();
 
-function detectLine(x, y) {
-  let imageData = ctx.getImageData(0, 0, 400, 400),
-    inputData = imageData.data,
-    pData = (~~x + ~~y * width) * 4;
+document.addEventListener("keydown", moveBox)
 
-  // console.log(imageData);
-  // console.log(inputData);
+let x = 350;
+let y = 350;
+let topleft, topright, botleft, botright;
+let px, py;
+let keyDown = false;
 
-  if (inputData[pData + 3]) {
-    return true;
-  }
-
-  return false;
+function moveBox(event){
+  let keyPressed = event.code;
+  px = x
+  py = y
+  if (keyPressed === "ArrowRight") {
+    x+=5; 
+  } else if (keyPressed === "ArrowLeft") {
+    x-=5;
+  } else if (keyPressed === "ArrowUp") {
+    y-=5;
+  } else if (keyPressed === "ArrowDown") {
+    y+=5;
+  } else if (keyPressed === "Space") {
+    x = 350;
+    y = 350;
+  } 
 }
 
-// function (e) just passes the "event" or "e" to our function to use. In this case its the action of moving one's mouse.
-cnv.addEventListener("mousemove", function (e) {
-  let x = e.pageX,
-    y = e.pageY;
-  console.log(detectLine(x, y));
-});
 
-let x, y;
-x = y = 300;
-
-document.addEventListener("keydown", (event) => {
-  let name = event.key;
-  let code = event.code;
-  if (name === "ArrowRight") {
-    x++;
-    console.log(x, y);
-  } else if (name === "ArrowLeft") {
-    x--;
-    console.log(x, y);
-  } else if (name === "ArrowUp") {
-    y++;
-    console.log(x, y);
-  } else if (name === "ArrowDown") {
-    y--;
-    console.log(x, y);
-  } else if (name === "Space") {
-    // idky it wont reset the variable to the original value
-    x = y = 300;
-  } else {
-    // Do nothing.
-    console.log(x, y);
-    return;
-  }
-  // draw after updating values
-  draw(x, y);
-});
 
 requestAnimationFrame(draw);
-function draw(x, y) {
+function draw() {
+
   ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, cnv.width, cnv.height);
+  ctx.fillRect(px, py, 50, 50);
 
   ctx.fillStyle = "blue";
   ctx.fillRect(x, y, 50, 50);
