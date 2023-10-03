720f695f8d93998e3426dd34a6148a76e5b1106e
Austin Bao <a.bao1@EDU.epsb.ca>
2023-10-03 09:48:13 -0600

    collision finally works

--- 05/Collision/Rectangles/main.js
+++ 05/Collision/Rectangles/main.js
@@ -1,9 +1,5 @@
 // COLLISION BY AUSTIN
 
-// https://www.jeffreythompson.org/collision-detection/index.php
-// https://happycoding.io/tutorials/processing/collision-detection
-// https://www.youtube.com/watch?v=_MyPLZSGS3s
-
 // GET CANVAS
 let cnv = document.getElementById("mycanvas");
 cnv.width = 400;
@@ -13,85 +9,93 @@ let ctx = cnv.getContext("2d");
 // LISTEN TO KEY BEING PRESSED
 document.addEventListener("keydown", moveBox);
 
-// DECLARING VARIABLES
-let x = 160,
-  y = 250;
-let px, py;
-
 function moveBox(event) {
+  // ALLOWS THE MOVEMENT OF THE BLACK BOX
   let keyPressed = event.code;
-  px = x;
-  py = y;
   if (keyPressed === "ArrowRight") {
-    x += 5;
+    boxX += 5;
   } else if (keyPressed === "ArrowLeft") {
-    x -= 5;
+    boxX -= 5;
   } else if (keyPressed === "ArrowUp") {
-    y -= 5;
+    boxY -= 5;
   } else if (keyPressed === "ArrowDown") {
-    y += 5;
+    boxY += 5;
   } else if (keyPressed === "Space") {
-    x = 160;
-    y = 250;
+    boxX = 160;
+    boxY = 250;
   }
 }
 
-function isCollide(boxDim) {
-  let xLeft = 150,
-    xRight = 220,
-    yBot = 220,
-    yTop = 150;
+function isCollide(rectangles, boxX, boxY, boxDim) {
+  // TO DETECT IF I TOUCH ANY RECTANGLE
+  for (rect in rectangles) {
+    let rect1Left = rectangles[rect].x,
+      rect1Right = rectangles[rect].x + rectangles[rect].width,
+      rect1Bot = rectangles[rect].y + rectangles[rect].height,
+      rect1Top = rectangles[rect].y;
 
-  let farleft = x,
-    farright = x + boxDim,
-    bottom = y + boxDim,
-    top = y;
+    let rect2Left = boxX,
+      rect2Right = boxX + boxDim,
+      rect2Bot = boxY + boxDim,
+      rect2Top = boxY;
 
-  if (bottom > yTop && top < yBot && farright > xLeft && farleft < xRight) {
-    return true;
+    if (
+      rect2Bot > rect1Top &&
+      rect2Top < rect1Bot &&
+      rect2Right > rect1Left &&
+      rect2Left < rect1Right
+    ) {
+      return true;
+    }
   }
-  return false;
 }
-let x1 = 130;
-let y1 = 120;
 
-let x2 = 200;
-let y2 = 150;
+// DECLARING VARIABLES + EASY TO ADD MORE COLLISION OBJECTS
+let rectangles = {
+  rect1: { x: 100, y: 120, width: 60, height: 60 },
+  rect2: { x: 200, y: 150, width: 60, height: 60 },
+  rect3: { x: 300, y: 200, width: 60, height: 60 },
+};
+
+let boxX = 160,
+  boxY = 300;
+
 requestAnimationFrame(draw);
 function draw() {
-  x1--;
-  x2--;
+  // RESET SQUARES WHEN OFF SCREEN
+  rectangles.rect1.x--;
+  rectangles.rect2.x--;
+  rectangles.rect3.x--;
 
-  let rectangles = [];
-
-  if (x1 + 60 < 0) {
-    x1 = cnv.width;
-    y1 = Math.random() * 200;
-    rectangles.push(x1, y1);
-    console.log(rectangles);
+  if (rectangles.rect1.x + 60 < 0) {
+    rectangles.rect1.x = cnv.width;
+    rectangles.rect1.y = Math.random() * 200;
+  }
+  if (rectangles.rect2.x + 60 < 0) {
+    rectangles.rect2.x = cnv.width;
+    rectangles.rect2.y = Math.random() * 200;
+  }
+  if (rectangles.rect3.x + 60 < 0) {
+    rectangles.rect3.x = cnv.width;
+    rectangles.rect3.y = Math.random() * 200;
   }
 
-  if (x2 + 60 < 0) {
-    x2 = cnv.width;
-    y2 = Math.random() * 200;
-    rectangles.push(x2, y2);
-    console.log(rectangles);
-  }
-
+  // CLEARS PREVIOUS BOXES
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, cnv.width, cnv.height);
 
+  // MOVING GREEN BOXS
   ctx.fillStyle = "green";
-  ctx.fillRect(150, 150, 70, 70);
+  ctx.fillRect(rectangles.rect1.x, rectangles.rect1.y, 60, 60);
+  ctx.fillRect(rectangles.rect2.x, rectangles.rect2.y, 60, 60);
+  ctx.fillRect(rectangles.rect3.x, rectangles.rect3.y, 60, 60);
 
-  // MOVING BOX
-  ctx.fillRect(x1, y1, 60, 60);
-  ctx.fillRect(x2, y2, 60, 60);
-
+  // CONTROLED BOX
   ctx.fillStyle = "black";
-  ctx.fillRect(x, y, 50, 50);
+  ctx.fillRect(boxX, boxY, 50, 50);
 
-  if (isCollide(50)) {
+  // CHANGES HTML ELEMENT DEPENDING IF BLACK BOX "COLLIDES" WITH GREEN BOXES
+  if (isCollide(rectangles, boxX, boxY, 50)) {
     document.getElementById("collide").innerHTML = "TRUE";
   } else {
     document.getElementById("collide").innerHTML = "FALSE";
