655bc791d043efd3cf830482b8aed84bb29896ee
Austin Bao <a.bao1@EDU.epsb.ca>
2023-10-23 09:53:16 -0600

    complete

--- 02/IPOCalculator/main.js
+++ 02/IPOCalculator/main.js
@@ -1,38 +1,106 @@
 // IPO CALC by Austin
 
-let formula = document
-  .getElementById("formula-select")
-  .addEventListener("change", displayFormula);
+// listens to changes to our selected value from the html "select" tag 
+let formula = document.getElementById("formula-select").addEventListener("change", displayFormula);
 
-let text = document.getElementById("name");
+// Set variable do I can save space and write less
+let result = document.getElementById("answer")
 
+document.getElementById("calculate").addEventListener("click", runFormula)
+
+let inputIds = [];
 function displayFormula(event) {
-  let formula = event.target.value;
-  let text = document.getElementById("name");
+  formula = event.target.value;
+  let info = document.getElementById("formula-info")
+  // reset input ids whenever we swtich to a new formula so we don't get old variables
+  inputIds.length = 0;
+  
+  // resets the result after switching formulas  
+  result.innerHTML = "-----"
 
-  text.textContent = "Formula: " + formula;
+  // Changes what the inputs and img depending on what formula was selected
+  if (formula === "nothing"){
+    info.innerHTML = `<img src="imgs/question-mark.png" alt="question mark" width="500" />`
+
+  } else if (formula === "distpoints"){
+    info.innerHTML = `<img src="imgs/distance-formula.png" alt="distance formula" width="500" />
+    <p>X1: <input type="number" id="x1"></p>
+    <p>Y1: <input type="number" id="y1"></p>
+    <p>X2: <input type="number" id="x2"></p>
+    <p>Y2: <input type="number" id="y2"></p>`
+    // push ids used for each input so we can access users input values later
+    inputIds.push("x1", "y1", "x2", "y2")
+
+  } else if (formula === "slope"){
+    info.innerHTML = `<img src="imgs/slope.png" alt="slope" width="500" />
+    <p>X1: <input type="number" id="x1"></p>
+    <p>Y1: <input type="number" id="y1"></p>
+    <p>X2: <input type="number" id="x2"></p>
+    <p>Y2: <input type="number" id="y2"></p>`
+    inputIds.push("x1", "y1", "x2", "y2")
+    
+  } else if (formula === "heron"){
+    info.innerHTML = `<img src="imgs/The-Herons-Formula.png" alt="heron's formula" width="500" />
+    <p>A: <input type="number" id="a"></p>
+    <p>B: <input type="number" id="b"></p>
+    <p>C: <input type="number" id="c"></p>`
+    inputIds.push("a", "b", "c")
+
+  } else if (formula === "traparea"){
+    info.innerHTML = `<img src="imgs/Area-of-a-Trapezoid.png" alt="Area of a Trapezoid" width="500" />
+    <p>A: <input type="number" id="a"></p>
+    <p>B: <input type="number" id="b"></p>
+    <p>H: <input type="number" id="h"></p>`
+    inputIds.push("a", "b", "h")
+  }
 }
 
-// function dist(x1, y1, x2, y2) {
-//   return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
-// }
 
-// function slope(x1, y1, x2, y2) {
-//   return (y2 - y2) / (x2 - x1);
-// }
+function runFormula(){
+// Create new list called values to store all the user's int inputs
+  let values = [];
+  // loop through all input ids to find all of user's input and push to values array
+  for (let i = 0; i < inputIds.length; i++) {
+    values.push(+document.getElementById(inputIds[i]).value)
+  }
 
-// function dist(x1, y1, x2, y2) {
-//   return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
-// }
+  // Determines which formula (function) to call
+  if (formula === "distpoints"){ 
+    // indexing values array to provide the input for each function
+    result.innerHTML = dist(values[0], values[1], values[2], values[3])
 
-// function heron(a, b, c) {
-//   let s = (a + b + c) / 2;
-//   return Math.sqrt(s * (s - a) * (s - b) * (s - c));
-// }
+  } else if (formula === "slope"){
+    result.innerHTML = slope(values[0], values[1], values[2], values[3])
 
-// function trapezoidArea(a, b, h) {
-//   return 0.5 * (a + b) * h;
-// }
+  } else if (formula === "heron"){
+    result.innerHTML = heron(values[0], values[1], values[2])
+
+  } else if (formula === "traparea"){
+    result.innerHTML = trapezoidArea(values[0], values[1], values[2])
+  }
+}
+
+// following functions all return a number
+function dist(x1, y1, x2, y2) {
+  return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+}
+
+function slope(x1, y1, x2, y2) {
+  return (y2 - y1) / (x2 - x1);
+}
+
+function heron(a, b, c) {
+  let s = (a + b + c) / 2;
+  console.log(a,b,c)
+  return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+}
+
+function trapezoidArea(a, b, h) {
+  return 0.5 * (a + b) * h;
+}
+
+
+// OTHER FUNCTIONS I CAN ADD 
 
 // function midpoint(x1, y1, x2, y2) {
 //   return [(x1 + x2) / 2, (y1 + y2) / 2];
